575d5a6cca95f540b91430c2de1a987db447b1be
Ellham Khan <ellhamkhan444@gmail.com>
2025-05-09 01:04:49 +0600

    header part complete

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,8 @@ import Popup from './Popup'
 import { FaFacebook, FaInstagramSquare, FaLinkedin, FaPinterest, FaTwitter, FaYoutube } from 'react-icons/fa'
 import LanguageCurrencySelector from './LanguageCurrencySelector'
 import CliconLogo from '/CliconLogo.png'
-import { Heart, Search, ShoppingCart, UserRound } from 'lucide-react'
+import { ChevronDown, Headphones, Heart, Info, MapPin, Phone, Search, ShoppingCart, UserRound } from 'lucide-react'
+import { IoIosGitCompare } from 'react-icons/io'
 
 const Header = () => {
     return (
@@ -37,23 +38,54 @@ const Header = () => {
                 </div>
             </header>
             <header className='bg-[#1B6392] border-t border-[#F2F4F5]'>
-                    <div className="max-w-[1320px] mx-auto">
-                        <div className="flex items-center justify-between py-5  ">
-                            <div className="">
-                                <img src={CliconLogo} alt="" className='cursor-pointer' />
-                            </div>
-                            <div className="flex items-center justify-between bg-white w-[653.5px] px-5 py-3.5">
-                                <input type="text" placeholder='Search for anything...' className='text-[#77878F] text-sm leading-5 w-full outline-none' />
-                                <Search size={20} color='#191C1F' className='cursor-pointer' />
-                            </div>
-                            <div className="flex items-center gap-x-6 text-white">
-                                <ShoppingCart size={32} className='cursor-pointer' />
-                                <Heart size={32} className='cursor-pointer' />
-                                <UserRound size={32} className='cursor-pointer' />
-                            </div>
+                <div className="max-w-[1320px] mx-auto">
+                    <div className="flex items-center justify-between py-5  ">
+                        <div className="">
+                            <img src={CliconLogo} alt="" className='cursor-pointer' />
+                        </div>
+                        <div className="flex items-center justify-between bg-white w-[653.5px] px-5 py-3.5">
+                            <input type="text" placeholder='Search for anything...' className='text-[#77878F] text-sm leading-5 w-full outline-none' />
+                            <Search size={20} color='#191C1F' className='cursor-pointer' />
+                        </div>
+                        <div className="flex items-center gap-x-6 text-white">
+                            <ShoppingCart size={32} className='cursor-pointer' />
+                            <Heart size={32} className='cursor-pointer' />
+                            <UserRound size={32} className='cursor-pointer' />
                         </div>
                     </div>
+                </div>
             </header>
+            <section className='flex items-center justify-between max-w-[1320px] mx-auto py-4'>
+                <div className="flex items-center gap-x-6">
+                    <div className="flex items-center gap-x-1.5 bg-[#F2F4F5] px-6 py-3.5 text-[#191C1F]">
+                        <span className='text-sm leading-5 font-medium'>All Category</span>
+                        <ChevronDown size={16} />
+                    </div>
+                    <div className="flex items-center gap-x-1.5 text-[#5F6C72]">
+                        <MapPin size={24} />
+                        <span className='text-sm leading-5 '>Track Order </span>
+
+                    </div>
+                    <div className="flex items-center gap-x-1.5 text-[#5F6C72]">
+                        <IoIosGitCompare size={24} />
+                        <span className='text-sm leading-5 '>
+                            Compare</span>
+                    </div>
+                    <div className="flex items-center gap-x-1.5 text-[#5F6C72]">
+                        <Headphones size={24} />
+                        <span className='text-sm leading-5 '>Customer Support</span>
+                    </div>
+                    <div className="flex items-center gap-x-1.5 text-[#5F6C72]">
+                        <Info size={24} />
+                        <span className='text-sm leading-5 '>Need Help</span>
+                    </div>
+
+                </div>
+                <div className="flex items-center gap-x-2 text-[#191C1F]">
+                <Phone />
+                <span className='text-lg leading-6'>+1-202-555-0104</span>
+                </div>
+            </section>
         </>
     )
 }
