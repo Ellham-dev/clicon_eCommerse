489085ca02122e7114b30791c5fcf3ef73e3ab9d
Ellham Khan <ellhamkhan444@gmail.com>
2025-05-14 22:07:21 +0600

    ShopCetagory components complete

--- src/components/ShopCategory.jsx
+++ src/components/ShopCategory.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
+import CategorySlider from './CategorySlider'
 
 const ShopCategory = () => {
   return (
     <>
       <section>
-        <div className="max-w-[1320px] mx-auto">
+        <div className="max-w-[1320px] mx-auto mb-[72px]">
           <div className="">
             <div className="text-center mb-10">
               <h4 className='text-[32px] leading-10 font-semibold text-[#141414]'>Shop with Categorys</h4>
             </div>
             <div className="">
-              
+              <CategorySlider/>
             </div>
           </div>
         </div>
